42d1af5656c22e3afe5c3d4ea972f7fe3f39daf6
nerea <nerea@ubuntu.myguest.virtualbox.org>
2024-03-06 16:47:34 +0100

    acabar diseño nagevador y retocar foto del background

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,9 +4,18 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// FONTAWESOME
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 const app = createApp(App)
 
+library.add(fas, far, fab)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
+
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,8 +1,30 @@
 <script setup lang="ts">
-
+import Menu from "@/Components/Menu.vue";
 </script>
 
-<template>
-<h1>Aegis</h1>
 
+<template>
+  <div class="background-image">
+    <Menu></Menu>
+    <h1>BIENVENIDOS A AEGIS</h1>
+   <p>Ciberseguridad para microempresas y autónomos</p>
+  </div>
 </template>
+
+<style scoped>
+.background-image {
+  background-image: url('../../public/fondoImagenPrincipal.jpeg');
+  background-size: cover;
+  background-position: center;
+  height: 90vh;
+}
+h1, p{
+    color: white;
+    text-align: center;
+    font-size: 67px;
+    font-family: Roboto,Helvetica,Arial,sans-serif;
+}
+p{
+    font-size: 18px;
+}
+</style>
