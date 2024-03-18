4d0ec558c3d19489bae9b52530480c0cf1cc8c14
CarlosTarriasDiazWeb <carlos.tarrias.diaz.web@gmail.com>
2024-03-18 15:51:30 +0100

    Added vue final modal

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,13 +2,17 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config';
 import 'primevue/resources/themes/aura-light-green/theme.css'
+import 'vue-final-modal/style.css';
+import { createVfm } from 'vue-final-modal';
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const vfm = createVfm();
 
 app.use(createPinia())
+app.use(vfm);
 app.use(router)
 app.use(PrimeVue);
 
