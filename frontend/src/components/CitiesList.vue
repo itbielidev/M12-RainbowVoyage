5908cfdd3a5abd787ea29975f9e8aedfb223068d
CarlosTarriasDiazWeb <carlos.tarrias.diaz.web@gmail.com>
2024-03-20 22:35:26 +0100

    CIties Api route finished

--- frontend/src/components/CitiesList.vue
+++ frontend/src/components/CitiesList.vue
@@ -2,13 +2,26 @@
   <NavBar></NavBar>
   <div class="container">
     <div class="row">
-      <div v-for="(city, index) in cities" :key="index" class="col-md-6 mb-3 position-relative text-center" style="overflow:hidden">
-        <div class="text-container w-100" style="margin-top: -200px;" v-if="!city.hovered">
-          <p class="m-0" style="color: black;">{{ city.name }}</p>
+      <div
+        v-for="(city, index) in hoveredCities"
+        :key="index"
+        class="col-md-6 mb-3 position-relative text-center"
+        style="overflow: hidden"
+      >
+        <div class="text-container w-100" style="margin-top: -200px" v-if="!city.hovered">
+          <p class="m-0" style="color: black">{{ city.name }}</p>
         </div>
-        <img :src="city.image" class="img-fluid" :alt="city.alt" @mouseover="hoverCity(index, true)" @mouseout="hoverCity(index, false)">
+        <img
+          :src="`/${city.images[0]}`"
+          class="img-fluid"
+          alt="City photo"
+          @mouseover="hoverCity(index, true)"
+          @mouseout="hoverCity(index, false)"
+        />
         <div class="overlay">
-          <p style="font-size: 20px;margin-left: 20px; margin-right: 20px;">{{ city.overlayText }}</p>
+          <p style="font-size: 20px; margin-left: 20px; margin-right: 20px">
+            {{ city.description }}
+          </p>
         </div>
       </div>
     </div>
@@ -16,19 +29,23 @@
 </template>
 
 <script setup lang="ts">
-import NavBar from "@/components/NavBar.vue";
+import NavBar from '@/components/NavBar.vue'
+import { useCitiesStore } from '@/stores/cities'
+import { storeToRefs } from 'pinia'
 
-const cities = [
-  { name: "BARCELONA", image: "../../public/Barcelona.png", alt: "Barcelona", overlayText: "Barcelona es conocida por su vibrante comunidad LGTBIQ, con eventos como el Barcelona Pride y una escena nocturna inclusiva que refleja su apoyo a la diversidad.", hovered: false },
-  { name: "MADRID", image: "../../public/Madrid.png", alt: "Madrid", overlayText: "Madrid destaca como un epicentro LGTBIQ en España, con barrios como Chueca que celebran la diversidad con orgullo y eventos como el WorldPride, atrayendo a personas de todo el mundo.", hovered: false },
-  { name: "IBIZA", image: "../../public/Ibiza.png", alt: "Ibiza", overlayText: "Ibiza ofrece un ambiente de apertura y aceptación, con fiestas inclusivas y espacios seguros para la comunidad LGTBIQ, convirtiéndola en un destino popular para personas de todas las orientaciones sexuales.", hovered: false },
-  { name: "MÁLAGA", image: "../../public/Malaga.png", alt: "Malaga", overlayText: "Málaga ofrece una atmósfera cosmopolita y tolerante, con una escena LGTBIQ vibrante y eventos como el Festival Internacional de Cine LGTBIQ+ que promueven la inclusión y la diversidad en la ciudad.", hovered: false },
-  { name: "GRAN CANARIAS", image: "../../public/GranCanarias.png", alt: "Gran Canarias",overlayText: "Gran Canaria es reconocida por su comunidad diversa y acogedora, especialmente en lugares como Maspalomas, que alberga eventos como el Maspalomas Gay Pride, atrayendo a visitantes de todas partes del mundo.", hovered: false }
-];
+const { hoveredCities } = storeToRefs(useCitiesStore())
+
+// const cities = [
+//   { name: "BARCELONA", image: "../../public/Barcelona.png", alt: "Barcelona", overlayText: "Barcelona es conocida por su vibrante comunidad LGTBIQ, con eventos como el Barcelona Pride y una escena nocturna inclusiva que refleja su apoyo a la diversidad.", hovered: false },
+//   { name: "MADRID", image: "../../public/Madrid.png", alt: "Madrid", overlayText: "Madrid destaca como un epicentro LGTBIQ en España, con barrios como Chueca que celebran la diversidad con orgullo y eventos como el WorldPride, atrayendo a personas de todo el mundo.", hovered: false },
+//   { name: "IBIZA", image: "../../public/Ibiza.png", alt: "Ibiza", overlayText: "Ibiza ofrece un ambiente de apertura y aceptación, con fiestas inclusivas y espacios seguros para la comunidad LGTBIQ, convirtiéndola en un destino popular para personas de todas las orientaciones sexuales.", hovered: false },
+//   { name: "MÁLAGA", image: "../../public/Malaga.png", alt: "Malaga", overlayText: "Málaga ofrece una atmósfera cosmopolita y tolerante, con una escena LGTBIQ vibrante y eventos como el Festival Internacional de Cine LGTBIQ+ que promueven la inclusión y la diversidad en la ciudad.", hovered: false },
+//   { name: "GRAN CANARIAS", image: "../../public/GranCanarias.png", alt: "Gran Canarias",overlayText: "Gran Canaria es reconocida por su comunidad diversa y acogedora, especialmente en lugares como Maspalomas, que alberga eventos como el Maspalomas Gay Pride, atrayendo a visitantes de todas partes del mundo.", hovered: false }
+// ];
 
 const hoverCity = (index: number, hovered: boolean) => {
-  cities[index].hovered = hovered;
-};
+  // hoveredCities.value[index]?.hovered = hovered
+}
 </script>
 
 <style>
@@ -70,7 +87,12 @@ img {
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.5); /* Cambiamos el degradado por un color sólido con transparencia */
+  background-color: rgba(
+    0,
+    0,
+    0,
+    0.5
+  ); /* Cambiamos el degradado por un color sólido con transparencia */
   opacity: 0;
   transition: opacity 0.3s ease;
   display: flex;
--- frontend/src/stores/cities.ts
+++ frontend/src/stores/cities.ts
@@ -1,18 +1,24 @@
 import { useStorage } from "@vueuse/core";
 import { defineStore } from "pinia";
-import { type Ref } from "vue";
-import type {City} from "@/types/index";
+import { computed, type Ref } from "vue";
+import type { City } from "@/types/index";
 import { useFetch } from "@/composables/useFetch";
 
-export const useAuthStore = defineStore('cities', () => {
+export const useCitiesStore = defineStore('cities', () => {
 
-    const {get} = useFetch<City[]>();
+    const { get } = useFetch<{ cities: City[] }>();
 
-    const cities : Ref<City[]|null> = useStorage("cities", [], sessionStorage);
+    const cities: Ref<City[] | null | undefined> = useStorage("cities", [], sessionStorage);
+
+    //To apply hover effect in main page.
+    const hoveredCities = computed(() => {
+        return cities.value?.map(city => ({ ...city, hovered: false }))
+    })
 
     async function getAll() {
-        cities.value = await get("/cities");
+        const citiesData = await get("/cities");
+        cities.value = citiesData?.cities;
     }
 
-    return { cities, getAll }
+    return { cities, getAll, hoveredCities }
 })
