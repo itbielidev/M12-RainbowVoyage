9af83cef14751160023345b13b7697d2a83c8d7c
nereatirado <nerea.tirado.7e6@itb.cat>
2024-03-21 16:50:36 +0100

    ruter experiencias detalle

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import TodosView from '@/views/TodosView.vue'
 import ExperiencesView from '../views/ExperiencesView.vue'
+import ExperienceDetail from '../views/ExperienceDetail.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +22,12 @@ const router = createRouter({
       name: 'experiences',
       component: ExperiencesView,
       props: route => ({ ...route.params, cityName: route.params.cityName })
+    },
+    {
+      path: '/experienceDetail/:experienceId',
+      name: 'experienceDetail',
+      component: ExperienceDetail,
+      props: route => ({ ...route.params, experienceId: route.params.experienceId })
     },
   ]
 })
---
+++ frontend/src/views/ExperienceDetail.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <NavBar></NavBar>
+      <div class="content">
+        <h1 style="color: white;">¡Bienvenido a Axel Hotel Barcelona, Tu Destino en Barcelona!</h1>
+      </div>
+      <FooterComponent></FooterComponent>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import NavBar from '@/components/NavBar.vue'
+  import FooterComponent from '@/components/FooterComponent.vue';
+  </script>
+  
+  <style scoped>
+  .content {
+    background-color: black;
+    padding: 20px;
+  }
+  </style>
+  
