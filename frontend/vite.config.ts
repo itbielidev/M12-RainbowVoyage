ce27905fb3b19e4ee8cdec8c7a1ad3afc697be71
CarlosTarriasDiazWeb <carlos.tarrias.diaz.web@gmail.com>
2024-04-03 16:30:54 +0200

    added pwa files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,23 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: false,
+    }),
   ],
   resolve: {
     alias: {
