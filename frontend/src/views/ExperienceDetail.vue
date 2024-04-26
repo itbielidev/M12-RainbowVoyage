dff385a65e3e66765532a256e3287e44f7f45f98
CarlosTarriasDiazWeb <carlos.tarrias.diaz.web@gmail.com>
2024-04-26 15:18:51 +0200

    fixed

--- frontend/src/views/ExperienceDetail.vue
+++ frontend/src/views/ExperienceDetail.vue
@@ -2,24 +2,24 @@
   <div class="body">
     <NavBar></NavBar>
     <section class="breadcrumbs-box container">
-          <BreadCrumbs :items="items"></BreadCrumbs>
+      <BreadCrumbs :items="items"></BreadCrumbs>
     </section>
     <div class="content">
       <h1>{{ experience?.city.name }}: {{ experience?.name }}</h1>
     </div>
-  
+
     <div class="container mt-5 text-center">
       <div class="row">
         <div class="col-md-6">
           <p class="exp-description">{{ experience?.descriptions[1] }}</p>
         </div>
-     
+
         <div class="col-md-6">
           <!-- Carrusel de Fotos -->
           <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-inner">
               <div
-                v-for="(h, index) in experience?.images"
+                v-for="(h, index) in experience?.images.slice(7)"
                 :key="index"
                 class="carousel-item"
                 :class="{ active: index === 0 }"
@@ -63,17 +63,6 @@
             <div>
               <h4 class="text-center fw-bold">Tu pack incluye</h4>
               <ul class="text-center includes" style="list-style-type: none">
-                <!-- <li>Asistencia a la llegada</li>
-                <li>4 noches</li>
-                <li>Todo incluido</li>
-                <li>Teléfono de emergencias 24h/365días.</li>
-                <li>
-                  REGALO:
-                  <ul style="list-style-type: none">
-                    <li>1 día de SPA</li>
-                  </ul>
-                </li>
-                <li>Rutas</li> -->
                 <li
                   v-for="(inc, index) in experience?.descriptions.slice(-1)[0].split('.')"
                   :key="index"
@@ -98,40 +87,7 @@
               </div>
             </div>
           </div>
-          <!-- <div class="circle">
-            <div class="contenidoRutas">
-              <p class="dia">DIA 1</p>
-              <p class="info">Casa Batlló visita guida de 16:30-18:00</p>
-            </div>
-          </div>
-          <div class="circle">
-            <div class="contenidoRutas">
-              <p class="dia">DIA 2</p>
-              <p class="info">Sagrada Familia visita guiada de 9:30-11:00</p>
-            </div>
-          </div>
-          <div class="circle">
-            <div class="contenidoRutas">
-              <p class="dia">DIA 3</p>
-              <p class="info">Excursión a Montserrat de 9:00-18:00</p>
-            </div>
-          </div> -->
         </div>
-        <!-- <div class="d-flex justify-content-center">
-          <div class="circle">
-            <div class="contenidoRutas">
-              <p class="dia">DIA 4</p>
-              <p class="info">Parc Güell viista guiada de 12:30-14:00</p>
-            </div>
-          </div>
-          <div class="circle">
-            <div class="contenidoRutas">
-              <p class="dia">DIA 5</p>
-              <p class="info">La Pedrera visita guiada de 15:00-17:00</p>
-              <p class="info">Paseo por la Ciutadella</p>
-            </div>
-          </div>
-        </div> -->
       </div>
       <button class="reserva d-none" type="submit">RESERVA</button>
     </div>
@@ -178,13 +134,11 @@ const people = ref<string>('')
 const items = ref([
   { label: 'Home', route: '/' },
   {
-    label: `Experiencias de ${experience.value?.city.name}`,
+    label: `Experiencias`,
     route: `/experiences/${experience.value?.city.name}`
   },
   { label: 'Detalle de la experiencia' }
 ])
-
-//var hotel = ['hotel.webp', 'habitación.webp', 'copas.webp', 'piscina.webp', 'spa.webp']
 
 onMounted(async () => {
   await getExperience(props.experienceId)
@@ -228,13 +182,13 @@ const { open, close } = useModal({
 
 <style scoped>
 .body {
-  background-color: #F8F9FA;
+  background-color: #f8f9fa;
 }
 
 .breadcrumbs-box {
   /* el margin-top tiene que estar a 9 rem sino se oculta detras del nav */
   margin-top: 9rem !important;
-  background-color: #F8F9FA
+  background-color: #f8f9fa;
 }
 
 .imagenDetalle {
