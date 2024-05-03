fa6f0044501d795b9ee429b6bd035df22c6e92c5
CarlosTarriasDiazWeb <carlos.tarrias.diaz.web@gmail.com>
2024-05-03 16:10:57 +0200

    types declared in types2pdf library

--- frontend/src/views/ReservationDetail.vue
+++ frontend/src/views/ReservationDetail.vue
@@ -9,84 +9,84 @@
           <h1>RESERVA</h1>
         </div>
         <div class="alinear">
-        <h3>Billetes de ida</h3>
-        <div class="informacion text-center">
-          <p class="fw-bolder">Nombre:</p>
-          <p>{{ reservation.name }}</p>
-          <p class="fw-bolder">Apellidos:</p>
-          <p>{{ reservation.name }}</p>
-          <p class="fw-bolder">DNI:</p>
-          <p>{{ reservation.name }}</p>
-        </div>
-        <div class="informacion">
-          <p class="fw-bolder">Aeropuerto de salida:</p>
-          <p>{{ reservation.airportIn }}</p>
-          <p class="fw-bolder">Fecha:</p>
-          <p>{{ reservation.dates }}</p>
-          <p class="fw-bolder">Hora:</p>
-          <p>{{ reservation.partidaFirstDay }}</p>
-          <p class="fw-bolder">Puerta:</p>
-          <p>{{ randomGate }}</p>
-        </div>
-        <div class="informacion">
-          <p class="fw-bolder">Aeropuerto de llegada:</p>
-          <p>{{ reservation.airportOut }}</p>
-          <p class="fw-bolder">Fecha:</p>
-          <p>{{ reservation.dates }}</p>
-          <p class="fw-bolder">Hora:</p>
-          <p>{{ reservation.llegadaFirstDay }}</p>
-        </div>
+          <h3>Billetes de ida</h3>
+          <div class="informacion text-center">
+            <p class="fw-bolder">Nombre:</p>
+            <p>{{ reservation.name }}</p>
+            <p class="fw-bolder">Apellidos:</p>
+            <p>{{ reservation.name }}</p>
+            <p class="fw-bolder">DNI:</p>
+            <p>{{ reservation.name }}</p>
+          </div>
+          <div class="informacion">
+            <p class="fw-bolder">Aeropuerto de salida:</p>
+            <p>{{ reservation.airportIn }}</p>
+            <p class="fw-bolder">Fecha:</p>
+            <p>{{ reservation.dates }}</p>
+            <p class="fw-bolder">Hora:</p>
+            <p>{{ reservation.partidaFirstDay }}</p>
+            <p class="fw-bolder">Puerta:</p>
+            <p>{{ randomGate }}</p>
+          </div>
+          <div class="informacion">
+            <p class="fw-bolder">Aeropuerto de llegada:</p>
+            <p>{{ reservation.airportOut }}</p>
+            <p class="fw-bolder">Fecha:</p>
+            <p>{{ reservation.dates }}</p>
+            <p class="fw-bolder">Hora:</p>
+            <p>{{ reservation.llegadaFirstDay }}</p>
+          </div>
 
-        <h3>Billetes de vuelta</h3>
-        <div class="informacion text-center">
-          <p class="fw-bolder">Nombre:</p>
-          <p>{{ reservation.name }}</p>
-          <p class="fw-bolder">Apellidos:</p>
-          <p>{{ reservation.name }}</p>
-          <p class="fw-bolder">DNI:</p>
-          <p>{{ reservation.name }}</p>
-        </div>
-        <div class="informacion">
-          <p class="fw-bolder">Aeropuerto de salida:</p>
-          <p>{{ reservation.airportIn }}</p>
-          <p class="fw-bolder">Fecha:</p>
-          <p>{{ reservation.dates }}</p>
-          <p class="fw-bolder">Hora:</p>
-          <p>{{ reservation.partidaLastDay }}</p>
-          <p class="fw-bolder">Puerta:</p>
-          <p>{{ randomGate2 }}</p>
-        </div>
-        <div class="informacion">
-          <p class="fw-bolder">Aeropuerto de llegada:</p>
-          <p>{{ reservation.airportOut }}</p>
-          <p class="fw-bolder">Fecha:</p>
-          <p>{{ reservation.dates }}</p>
-          <p class="fw-bolder">Hora:</p>
-          <p>{{ reservation.llegadaLastDay }}</p>
-        </div>
-        <div class="mt-3 d-flex justify-content-around fotos">
-          <div class="parte col-lg-4 col-md-4 col-xs-12">
-            <font-awesome-icon icon="fa-solid fa-location-dot" class="city" /><br />
-            <img
-              class="mt-4 rounded-5"
-              :style="{ maxHeight: '200px', width: 'auto' }"
-              :src="`/images/${reservation.experience?.city.images[0]}`"
-            />
+          <h3>Billetes de vuelta</h3>
+          <div class="informacion text-center">
+            <p class="fw-bolder">Nombre:</p>
+            <p>{{ reservation.name }}</p>
+            <p class="fw-bolder">Apellidos:</p>
+            <p>{{ reservation.name }}</p>
+            <p class="fw-bolder">DNI:</p>
+            <p>{{ reservation.name }}</p>
           </div>
-          <div class="parte col-lg-4 col-md-4 col-xs-12">
-            <font-awesome-icon icon="fa-solid fa-route" class="routes" /><br />
-            <p class="rutas">{{ reservation.experience?.name }}</p>
+          <div class="informacion">
+            <p class="fw-bolder">Aeropuerto de salida:</p>
+            <p>{{ reservation.airportIn }}</p>
+            <p class="fw-bolder">Fecha:</p>
+            <p>{{ reservation.dates }}</p>
+            <p class="fw-bolder">Hora:</p>
+            <p>{{ reservation.partidaLastDay }}</p>
+            <p class="fw-bolder">Puerta:</p>
+            <p>{{ randomGate2 }}</p>
           </div>
-          <div class="parte col-lg-4 col-md-4 col-xs-12">
-            <font-awesome-icon icon="fa-solid fa-hotel" class="hotel" /><br />
-            <img
-              class="mt-4 rounded-5"
-              :style="{ maxHeight: '200px', width: 'auto' }"
-              :src="`/images/${reservation.experience?.city.name}/${reservation.experience?.images[7]}`"
-            />
+          <div class="informacion">
+            <p class="fw-bolder">Aeropuerto de llegada:</p>
+            <p>{{ reservation.airportOut }}</p>
+            <p class="fw-bolder">Fecha:</p>
+            <p>{{ reservation.dates }}</p>
+            <p class="fw-bolder">Hora:</p>
+            <p>{{ reservation.llegadaLastDay }}</p>
+          </div>
+          <div class="mt-3 d-flex justify-content-around fotos">
+            <div class="parte col-lg-4 col-md-4 col-xs-12">
+              <font-awesome-icon icon="fa-solid fa-location-dot" class="city" /><br />
+              <img
+                class="mt-4 rounded-5"
+                :style="{ maxHeight: '200px', width: 'auto' }"
+                :src="`/images/${reservation.experience?.city.images[0]}`"
+              />
+            </div>
+            <div class="parte col-lg-4 col-md-4 col-xs-12">
+              <font-awesome-icon icon="fa-solid fa-route" class="routes" /><br />
+              <p class="rutas">{{ reservation.experience?.name }}</p>
+            </div>
+            <div class="parte col-lg-4 col-md-4 col-xs-12">
+              <font-awesome-icon icon="fa-solid fa-hotel" class="hotel" /><br />
+              <img
+                class="mt-4 rounded-5"
+                :style="{ maxHeight: '200px', width: 'auto' }"
+                :src="`/images/${reservation.experience?.city.name}/${reservation.experience?.images[7]}`"
+              />
+            </div>
           </div>
         </div>
-      </div>
         <img src="../../public/images/logo.webp" width="200px" class="img-fluid mx-auto d-block" />
       </div>
     </section>
@@ -146,14 +146,14 @@ const printPDF = () => {
   const element = document.querySelector('.background')
 
   if (element) {
-    const contentToPrint = element.cloneNode(true)
-    const printButton = contentToPrint.querySelector('.print-button-container')
+    const contentToPrint: any = element.cloneNode(true)
+    const printButton: any = contentToPrint.querySelector('.print-button-container')
 
     if (printButton) {
       printButton.remove()
     }
 
-    const printButtonContainer = document.querySelector('.print-button-container')
+    const printButtonContainer: any = document.querySelector('.print-button-container')
     if (printButtonContainer) {
       printButtonContainer.style.display = 'none'
     }
@@ -175,7 +175,7 @@ const printPDF = () => {
           printButtonContainer.style.display = 'flex'
         }
       })
-      .catch((error) => {
+      .catch((error: any) => {
         console.error('Error al generar el PDF:', error)
         if (printButtonContainer) {
           printButtonContainer.style.display = 'flex'
@@ -186,11 +186,11 @@ const printPDF = () => {
 </script>
 
 <style scoped>
-.alinear{
+.alinear {
   margin-left: 25px;
   margin-top: 25px;
 }
-.rutas{
+.rutas {
   font-size: 25px;
   font-weight: bolder;
   margin-top: 90px;
@@ -198,7 +198,7 @@ const printPDF = () => {
 .parte {
   text-align: center; /* Para centrar el contenido de cada parte */
 }
-.fotos{
+.fotos {
   margin-bottom: 15px;
 }
 .cuerpo {
